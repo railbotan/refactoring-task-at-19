6691d2bdbd35138c28599d791cb0e8e4e2139434
Tripletri <Tripletri@yandex.ru>
2021-11-09 13:30:09 +0500

    Исправил алгоритм

--- filter.py
+++ filter.py
@@ -1,28 +1,29 @@
 from PIL import Image
 import numpy as np
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+image_data = np.array(img)
+height = len(image_data)
+width = len(image_data[1])
 i = 0
-while i < a - 11:
+while i < height:
     j = 0
-    while j < a1 - 11:
+    while j < width:
         s = 0
         for n in range(i, i + 10):
             for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
+                r = image_data[n][n1][0]
+                g = image_data[n][n1][1]
+                b = image_data[n][n1][2]
+                average_color = (int(r) + int(g) + int(b)) / 3
+                s += average_color
         s = int(s // 100)
         for n in range(i, i + 10):
             for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
+                image_data[n][n1][0] = int(s // 50) * 50
+                image_data[n][n1][1] = int(s // 50) * 50
+                image_data[n][n1][2] = int(s // 50) * 50
         j = j + 10
     i = i + 10
-res = Image.fromarray(arr)
+res = Image.fromarray(image_data)
 res.save('res.jpg')
