401611dccc86b90f009cdf7906cd63dcf1e8d6c6
Tripletri <Tripletri@yandex.ru>
2021-11-10 00:01:14 +0500

    Переход на матричные преобразования

--- filter.py
+++ filter.py
@@ -12,28 +12,21 @@ class ImageProcessor:
     def process_image(self):
         height = len(self.data)
         width = len(self.data[1])
-        for i in range(0, height, 10):
-            for j in range(0, width, 10):
-                color_sum = self._get_average_color(i, j)
-                self._set_gradation(color_sum, i, j)
+        for x in range(0, width, self.size):
+            for y in range(0, height, self.size):
+                average_color = self._get_average_color(x, y)
+                self._set_gradation(average_color, x, y)
         return Image.fromarray(self.data)
 
     def _get_average_color(self, start_x, start_y):
-        color_sum = 0
-        for x in range(start_x, start_x + self.size):
-            for y in range(start_y, start_y + self.size):
-                pixel = self.data[x][y]
-                average_color = pixel.sum() / 3
-                color_sum += average_color
-        color_sum = int(color_sum // self.size ** 2)
-        return color_sum
+        average_color_sum = self.data[start_x:start_x + self.size,
+                                      start_y:start_y + self.size].sum() / 3
+        average_color = int(average_color_sum // self.size ** 2)
+        return average_color
 
-    def _set_gradation(self, color_sum, i, j):
-        for x in range(i, i + self.size):
-            for y in range(j, j + self.size):
-                self.data[x][y][0] = int(color_sum // self.step) * self.step
-                self.data[x][y][1] = int(color_sum // self.step) * self.step
-                self.data[x][y][2] = int(color_sum // self.step) * self.step
+    def _set_gradation(self, color, start_x, start_y):
+        self.data[start_x:start_x + self.size,
+                  start_y:start_y + self.size] = int(color // self.step) * self.step
 
 
 img = Image.open("img2.jpg")
