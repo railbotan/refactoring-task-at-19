776bff4d7db8a46ce8929f98f85a522a530473ef
Arzu Rzaeva <rzaeva-010203@mail.ru>
2021-11-12 18:45:50 +0500

    Second step
    1. Code refactoring was carried out taking into account PEP8
    2. Added the ability to control the size of the mosaic
    3. Created function make_gray_picture
    4. Added the ability to control grayscale

--- filter.py
+++ filter.py
@@ -1,28 +1,36 @@
 from PIL import Image
 import numpy as np
+
+
+def make_gray_picture(img_arr, mosaic_size, grayscale):
+    height = len(img_arr)
+    width = len(img_arr[0])
+    gradation_step = 255 / (grayscale - 1)
+    y = 0
+    while y <= height - mosaic_size:
+        x = 0
+        while x <= width - mosaic_size:
+            aver_bright = 0
+            for px_y in range(y, y + mosaic_size):
+                for px_x in range(x, x + mosaic_size):
+                    red = img_arr[px_y][px_x][0]
+                    green = img_arr[px_y][px_x][1]
+                    blue = img_arr[px_y][px_x][2]
+                    px_median = (int(red) + int(green) + int(blue)) / 3
+                    aver_bright += px_median
+            aver_bright = int(aver_bright // (mosaic_size * mosaic_size))
+            for px_y in range(y, y + mosaic_size):
+                for px_x in range(x, x + mosaic_size):
+                    img_arr[px_y][px_x][0] = int(aver_bright // gradation_step) * gradation_step
+                    img_arr[px_y][px_x][1] = int(aver_bright // gradation_step) * gradation_step
+                    img_arr[px_y][px_x][2] = int(aver_bright // gradation_step) * gradation_step
+            x += mosaic_size
+        y += mosaic_size
+    return img_arr
+
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 11:
-    j = 0
-    while j < a1 - 11:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+img_array = np.array(img)
+make_gray_picture(img_array, 10, 5)
+res = Image.fromarray(img_array)
 res.save('res.jpg')
