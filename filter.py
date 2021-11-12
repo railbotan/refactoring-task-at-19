a8025fed4426cbb86e9f1c6cf5f5d04f6dc2f8b0
Arzu Rzaeva <rzaeva-010203@mail.ru>
2021-11-12 19:20:32 +0500

    Third step
    1. Replaced manual loops with matrix transformations

--- filter.py
+++ filter.py
@@ -10,20 +10,9 @@ def make_gray_picture(img_arr, mosaic_size, grayscale):
     while y <= height - mosaic_size:
         x = 0
         while x <= width - mosaic_size:
-            aver_bright = 0
-            for px_y in range(y, y + mosaic_size):
-                for px_x in range(x, x + mosaic_size):
-                    red = img_arr[px_y][px_x][0]
-                    green = img_arr[px_y][px_x][1]
-                    blue = img_arr[px_y][px_x][2]
-                    px_median = (int(red) + int(green) + int(blue)) / 3
-                    aver_bright += px_median
-            aver_bright = int(aver_bright // (mosaic_size * mosaic_size))
-            for px_y in range(y, y + mosaic_size):
-                for px_x in range(x, x + mosaic_size):
-                    img_arr[px_y][px_x][0] = int(aver_bright // gradation_step) * gradation_step
-                    img_arr[px_y][px_x][1] = int(aver_bright // gradation_step) * gradation_step
-                    img_arr[px_y][px_x][2] = int(aver_bright // gradation_step) * gradation_step
+            sum_bright = np.sum((img_arr[y: y + mosaic_size, x: x + mosaic_size]) / 3)
+            aver_bright = int(sum_bright // (mosaic_size * mosaic_size))
+            img_arr[y: y + mosaic_size, x: x + mosaic_size] = int(aver_bright // gradation_step) * gradation_step
             x += mosaic_size
         y += mosaic_size
     return img_arr
