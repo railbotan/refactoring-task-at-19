40b8bbdc4260238db34daa733a8e9a8571e17f30
ArtyomMoiseev <moiseev.artlenalex@gmail.com>
2021-11-13 20:43:30 +0500

    Этап № 3
    1. Заменил циклы, используя функционал numpy.

--- filter.py
+++ filter.py
@@ -1,42 +1,31 @@
 from PIL import Image
 import numpy as np
 
+
 class pixelArtGenerator():
 
     def __init__(self, gradations= 6, pixel_step = 10):
         self.step = pixel_step
         self.gradation = 255 / gradations
 
-    def get_pixel_color(self, arr, i, j):
-        color_sum = 0
-        for n in range(i, i + self.step):
-                for m in range(j, j + self.step):
-                    R = arr[n][m][0]
-                    G = arr[n][m][1]
-                    B = arr[n][m][2]
-                    color_sum += int(R) + int(G) + int(B)
-        return color_sum
+    def get_pixel_color(self, img_array, i, j):
+        return np.sum(img_array[i: i + self.step, j: j + self.step]) // (10 * self.step)
 
-    def set_pixel_color(self, arr, i, j, s):
-        for n in range(i, i + self.step):
-                for m in range(j, j + self.step):
-                    arr[n][m][0] = int(s // self.gradation) * self.gradation / 3
-                    arr[n][m][1] = int(s // self.gradation) * self.gradation / 3
-                    arr[n][m][2] = int(s // self.gradation) * self.gradation / 3
-        return arr
+    def set_pixel_color(self, img_array, i, j, s):
+        img_array[i: i + self.step, j: j + self.step] = int(s // self.gradation) * self.gradation / 3
+        return img_array 
 
     def generate(self, img):
-        arr = np.array(img)
-        h, w = len(arr), len(arr[1])
+        img_array = np.array(img)
+        h, w = len(img_array), len(img_array[1])
         for i in range(0, h, self.step):
             for j in range(0, w, self.step):
-                color_sum = self.get_pixel_color(arr, i, j) // (10 * self.step)
-                arr = self.set_pixel_color(arr, i, j, color_sum)
-        return Image.fromarray(arr)
-
+                color = self.get_pixel_color(img_array, i, j)
+                img_array = self.set_pixel_color(img_array, i, j, color)
+        return Image.fromarray(img_array)
 
 
 img = Image.open("img2.jpg")
-generator = pixelArtGenerator(gradations = 4)
+generator = pixelArtGenerator(gradations = 10, pixel_step=10)
 res = generator.generate(img)
 res.save('res.jpg')
