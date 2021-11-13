086d352bfbcf3bccfe1a0e46cd64fb3dc326f56c
nemoguigrat <sharos185@gmail.com>
2021-11-13 14:41:58 +0500

    Stage 3

--- filter.py
+++ filter.py
@@ -10,24 +10,19 @@ class GrayMosaicFilter:
 
         height = len(self.image_array)
         width = len(self.image_array[1])
-        for image_y in range(0, height, self.mosaic_size):
-            for image_x in range(0, width, self.mosaic_size):
-                brightness_average = self._get_brightness_average(image_x, image_y)
-                self._set_pixel_color(image_x, image_y, brightness_average)
+        for x in range(0, width, self.mosaic_size):
+            for y in range(0, height, self.mosaic_size):
+                brightness_average = self._get_brightness_average(x, y)
+                self._set_pixel_color(x, y, brightness_average)
         return self.image_array
 
     def _get_brightness_average(self, x, y):
-        brightness_average = 0
-        for area_y in range(y, y + self.mosaic_size):
-            for area_x in range(x, x + self.mosaic_size):
-                pixel_average = sum(list(map(int, self.image_array[area_y][area_x]))) / 3
-                brightness_average += pixel_average
-        return int(brightness_average // self.mosaic_size ** 2)
+        return int(
+            (self.image_array[x: x + self.mosaic_size, y: y + self.mosaic_size].sum()) / 3 // self.mosaic_size ** 2)
 
     def _set_pixel_color(self, x, y, average):
-        for area_y in range(y, y + self.mosaic_size):
-            for area_x in range(x, x + self.mosaic_size):
-                self.image_array[area_y][area_x] = [int(average // self.gradation) * self.gradation] * 3
+        self.image_array[x: x + self.mosaic_size, y: y + self.mosaic_size] = int(average // self.gradation) * \
+                                                                             self.gradation
 
 
 if __name__ == "__main__":
