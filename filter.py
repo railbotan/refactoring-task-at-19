3c7f94e4c3a02ed7f77124ad2e292a5beee99323
ArtyomMoiseev <moiseev.artlenalex@gmail.com>
2021-11-13 15:37:32 +0500

    Этап № 2
    1. Доработка кода под PEP8
    2. Выделение методов
    3. Возможность выбора размера ячейки, градации.

--- filter.py
+++ filter.py
@@ -1,27 +1,42 @@
 from PIL import Image
 import numpy as np
+
+class pixelArtGenerator():
+
+    def __init__(self, gradations= 6, pixel_step = 10):
+        self.step = pixel_step
+        self.gradation = 255 / gradations
+
+    def get_pixel_color(self, arr, i, j):
+        color_sum = 0
+        for n in range(i, i + self.step):
+                for m in range(j, j + self.step):
+                    R = arr[n][m][0]
+                    G = arr[n][m][1]
+                    B = arr[n][m][2]
+                    color_sum += int(R) + int(G) + int(B)
+        return color_sum
+
+    def set_pixel_color(self, arr, i, j, s):
+        for n in range(i, i + self.step):
+                for m in range(j, j + self.step):
+                    arr[n][m][0] = int(s // self.gradation) * self.gradation / 3
+                    arr[n][m][1] = int(s // self.gradation) * self.gradation / 3
+                    arr[n][m][2] = int(s // self.gradation) * self.gradation / 3
+        return arr
+
+    def generate(self, img):
+        arr = np.array(img)
+        h, w = len(arr), len(arr[1])
+        for i in range(0, h, self.step):
+            for j in range(0, w, self.step):
+                color_sum = self.get_pixel_color(arr, i, j) // (10 * self.step)
+                arr = self.set_pixel_color(arr, i, j, color_sum)
+        return Image.fromarray(arr)
+
+
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-h, w = len(arr), len(arr[1])
-i = 0
-while i < h:
-    j = 0
-    while j < w:
-        s = 0
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                R = arr[n][m][0]
-                G = arr[n][m][1]
-                B = arr[n][m][2]
-                M = int(R) + int(G) + int(B)
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                arr[n][m][0] = int(s // 50) * 50 / 3
-                arr[n][m][1] = int(s // 50) * 50 / 3
-                arr[n][m][2] = int(s // 50) * 50 / 3
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+generator = pixelArtGenerator(gradations = 4)
+res = generator.generate(img)
 res.save('res.jpg')
