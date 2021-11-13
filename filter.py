186f042049283e4386a768c698997aec94806ceb
Evita Inokova <gengerw1604@gmail.com>
2021-11-13 20:33:41 +0300

    task2

--- filter.py
+++ filter.py
@@ -2,27 +2,42 @@ from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
 arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n0 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = (int(n0) + int(n2) + int(n3)) / 3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+height = len(arr)
+width = len(arr[1])
+size = int(input("Enter size: "))
+gradation = int(input("Enter gradation: "))
+step = int(255 / gradation)
+
+class Grey:
+  def __init__(self, step, height, width, size, arr):
+    self.step = step
+    self.height = height
+    self.width = width
+    self.size = size
+    self.arr = arr
+  def getGrey(self):
+    i = 0
+    while i < self.height:
+      j = 0
+      while j < self.width:
+        average = 0
+        for n in range(i, i + self.size):
+            for n1 in range(j, j + self.size):
+                r = self.arr[n][n1][0]
+                g = self.arr[n][n1][1]
+                b = self.arr[n][n1][2]
+                grey = (int(r) + int(g) + int(b)) / 3
+                average += grey
+        average = int(average // (self.size*self.size))
+        for n in range(i, i + self.size):
+            for n1 in range(j, j + self.size):
+                self.arr[n][n1][0] = int(average // self.step) * self.step
+                self.arr[n][n1][1] = int(average // self.step) * self.step
+                self.arr[n][n1][2] = int(average // self.step) * self.step
+            j = j + self.size
+            i = i + self.size
+        return self.arr
+
+        newPicture = Grey(step, height, width, size, arr)
+        res = Image.fromarray(newPicture.getGrey())
+        res.save('res.jpg')
