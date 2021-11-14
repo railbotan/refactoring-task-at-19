f8b04a549f2cd5d77e3eddc6198bc3381c1a551e
frog016 <mixalyov.nikita@mail.ru>
2021-11-14 18:49:39 +0500

    Refactoring (Step 2)
    Renamed variables, highlighted the gradation and mosaic size parameters, highlighted functions.

--- filter.py
+++ filter.py
@@ -1,29 +1,37 @@
 from PIL import Image
 import numpy as np
 
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                r = arr[n][n1][0]
-                g = arr[n][n1][1]
-                b = arr[n][n1][2]
-                M = (int(r) + int(g) + int(b)) // 3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+def calculate_gray_cell(row, column):
+    gray_cell = 0
+    for x in range(row, min(row + cell_size, rows_count)):
+        for y in range(column, min(column + cell_size, columns_count)):
+            gray_cell += sum(pixels_matrix[x][y]) // 3
+    return int(gray_cell // (cell_size ** 2))
+
+
+def set_gray_in_cell(row, column):
+    for x in range(row, min(row + cell_size, rows_count)):
+        for y in range(column, min(column + cell_size, columns_count)):
+            for channel in range(0, 3):
+                pixels_matrix[x][y][channel] = int(gray // gradation) * gradation
+
+
+input_data = input("Enter cell size and gradation\n").split()
+image = Image.open("img2.jpg")
+cell_size = int(input_data[0])
+gradation = 255 // int(input_data[1])
+pixels_matrix = np.array(image)
+rows_count = len(pixels_matrix)
+columns_count = len(pixels_matrix[1])
+
+current_row = 0
+while current_row < rows_count:
+    current_column = 0
+    while current_column < columns_count:
+        gray = calculate_gray_cell(current_row, current_column)
+        set_gray_in_cell(current_row, current_column)
+        current_column = current_column + cell_size
+    current_row = current_row + cell_size
+
+Image.fromarray(pixels_matrix).save("res.jpg")
