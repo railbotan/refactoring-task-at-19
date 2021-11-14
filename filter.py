7e7572aa546a8fd2f64ee40092928b356bb81751
Soblev515 <alex.sobolev515@yandex.ru>
2021-11-14 11:54:39 +0500

    Этап 2
    -PEP8.
    -Именование переменных.
    -Возможность управлять размерами мозайки (сейчас — только 10x10).
    -Возможность управлять градациями серого (сейчас — с шагом 50). Лучше сделать просто в виде задания количества шагов. Например: 4 градации, 6 градаций.
    -Выделение функций(методов).

--- filter.py
+++ filter.py
@@ -1,28 +1,45 @@
 from PIL import Image
 import numpy as np
+class Filter():
+    def __init__(self, img, mosaic_size, grayscale):
+        self.arr = np.array(img)
+        self.mosaic_size = mosaic_size
+        self.grayscale = grayscale
+        
+    def __GetAverage(self, y, x):
+        average = 0
+        for n in range(y, y + self.mosaic_size):
+            for m in range(x, x + self.mosaic_size):
+                R = int(self.arr[n][m][0])
+                G = int(self.arr[n][m][1])
+                B = int(self.arr[n][m][2])
+                median = R + G + B
+                average += median
+        return int(average // (self.mosaic_size*self.mosaic_size))
+    
+    def __DoGray(self, y, x, average):
+        for n in range(y, y + self.mosaic_size):
+            for m in range(x, x + self.mosaic_size):
+                self.arr[n][m][0] = int(average // self.grayscale) * self.grayscale/3
+                self.arr[n][m][1] = int(average // self.grayscale) * self.grayscale/3
+                self.arr[n][m][2] = int(average // self.grayscale) * self.grayscale/3
+
+    def __SaveResult(self):
+        res = Image.fromarray(self.arr)
+        res.save('res.jpg')
+                
+    def StartFilter(self):
+        height, width = len(self.arr), len(self.arr[1])
+        y = 0
+        while y < height:
+            x = 0
+            while x < width:
+                average = self.__GetAverage(y, x)
+                self.__DoGray(y, x, average)
+                x = x + self.mosaic_size
+            y = y + self.mosaic_size
+        self.__SaveResult()
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-h = len(arr)
-w = len(arr[1])
-i = 0
-while i < h:
-    j = 0
-    while j < w:
-        s = 0
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                R = int(arr[n][m][0])
-                G = int(arr[n][m][1])
-                B = int(arr[n][m][2])
-                M = R + G + B
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                arr[n][m][0] = int(s // 50) * 50/3
-                arr[n][m][1] = int(s // 50) * 50/3
-                arr[n][m][2] = int(s // 50) * 50/3
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+filtr = Filter(img, 10, 50)
+filtr.StartFilter()
