f8051d7cfed545eec84ddee61a0a864d65632d42
KyreOn <kireonbk@gmail.com>
2021-11-15 18:42:59 +0500

    2 этап
    1. PEP8
    2. Изменены названия переменных
    3. Добавлена возможность управления размерами мозаики(пикселей) и градацией серого
    4. Выделены функции

--- filter.py
+++ filter.py
@@ -1,28 +1,42 @@
 from PIL import Image
 import numpy as np
+
+
+def get_average(x, y, pixel_size):
+    average = 0
+    for n in range(y, y + pixel_size):
+        for m in range(x, x + pixel_size):
+            r = img_arr[n][m][0]
+            g = img_arr[n][m][1]
+            b = img_arr[n][m][2]
+            brightness_mid = (int(r) + int(g) + int(b)) / 3
+            average += brightness_mid
+    return int(average // (pixel_size * pixel_size))
+
+
+def make_gray(x, y, pixel_size, grayscale):
+    average = get_average(x, y, pixel_size)
+    for n in range(y, y + pixel_size):
+        for m in range(x, x + pixel_size):
+            img_arr[n][m][0] = int(average // grayscale) * grayscale
+            img_arr[n][m][1] = int(average // grayscale) * grayscale
+            img_arr[n][m][2] = int(average // grayscale) * grayscale
+
+
+def convert(arr, pixel_size, grayscale):
+    height = len(arr)
+    width = len(arr[1])
+    y = 0
+    while y < height:
+        x = 0
+        while x < width:
+            make_gray(x, y, pixel_size, grayscale)
+            x = x + 10
+        y = y + 10
+        res = Image.fromarray(arr)
+        res.save('res.jpg')
+
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                n1 = arr[n][m][0]
-                n2 = arr[n][m][1]
-                n3 = arr[n][m][2]
-                M = (int(n1) + int(n2) + int(n3))/3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                arr[n][m][0] = int(s // 50) * 50
-                arr[n][m][1] = int(s // 50) * 50
-                arr[n][m][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+img_arr = np.array(img)
+convert(img_arr, 10, 50)
