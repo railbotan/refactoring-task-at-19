6c38f1fda765661e8fadef21972179dc139e760d
KyreOn <kireonbk@gmail.com>
2021-11-15 18:49:40 +0500

    3 этап
    1. Изменена сигнатура метода convert
    2. Ручные циклы заменены на матричные преобразования

--- filter.py
+++ filter.py
@@ -3,40 +3,24 @@ import numpy as np
 
 
 def get_average(x, y, pixel_size):
-    average = 0
-    for n in range(y, y + pixel_size):
-        for m in range(x, x + pixel_size):
-            r = img_arr[n][m][0]
-            g = img_arr[n][m][1]
-            b = img_arr[n][m][2]
-            brightness_mid = (int(r) + int(g) + int(b)) / 3
-            average += brightness_mid
+    average = np.sum((img_arr[y: y + pixel_size, x: x + pixel_size])) / 3
     return int(average // (pixel_size * pixel_size))
 
 
 def make_gray(x, y, pixel_size, grayscale):
     average = get_average(x, y, pixel_size)
-    for n in range(y, y + pixel_size):
-        for m in range(x, x + pixel_size):
-            img_arr[n][m][0] = int(average // grayscale) * grayscale
-            img_arr[n][m][1] = int(average // grayscale) * grayscale
-            img_arr[n][m][2] = int(average // grayscale) * grayscale
+    img_arr[y: y + pixel_size, x: x + pixel_size] = int(average // grayscale) * grayscale
 
 
-def convert(arr, pixel_size, grayscale):
-    height = len(arr)
-    width = len(arr[1])
-    y = 0
-    while y < height:
-        x = 0
-        while x < width:
+def convert(pixel_size, grayscale):
+    height, width = len(img_arr), len(img_arr[1])
+    for y in range(0, height, pixel_size):
+        for x in range(0, width, pixel_size):
             make_gray(x, y, pixel_size, grayscale)
-            x = x + 10
-        y = y + 10
-        res = Image.fromarray(arr)
-        res.save('res.jpg')
+    res = Image.fromarray(img_arr)
+    res.save('res.jpg')
 
 
 img = Image.open("img2.jpg")
 img_arr = np.array(img)
-convert(img_arr, 10, 50)
+convert(10, 50)
