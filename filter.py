b86174fe7cf6a807af4582c3227602ea77f9cfbc
alinaskukina <alinaskukina@yandex.ru>
2021-11-21 13:52:51 +0500

    этап 2
    сделала рефакторинг кода через функции

--- filter.py
+++ filter.py
@@ -1,29 +1,41 @@
 from PIL import Image
 import numpy as np
+from numpy import ndarray
+
+class CreateImage:
+    def __init__(self, image_data: ndarray, size: int, level: int):
+        self.data = image_data
+        self.size = size
+        self.step = int(255 / level)
+
+    def get_color(self, start_x, start_y):
+        summa_color = 0
+        for x in range(start_x, start_x + self.size):
+            for y in range(start_y, start_y + self.size):
+                pixel = self.data[x][y]
+                color = pixel.sum() / 3
+                summa_color += color
+        summa_color = int(summa_color // self.size ** 2)
+        return summa_color
+
+    def create_image(self):
+        height = len(self.data)
+        width = len(self.data[1])
+        for i in range(0, height, 10):
+            for j in range(0, width, 10):
+                summa_color = self.get_color(i, j)
+                self.gradation(summa_color, i, j)
+        return Image.fromarray(self.data)
+
+    def gradation(self, summa_color, i, j):
+        for x in range(i, i + self.size):
+            for y in range(j, j + self.size):
+                self.data[x][y][0] = int(summa_color // self.step) * self.step
+                self.data[x][y][1] = int(summa_color // self.step) * self.step
+                self.data[x][y][2] = int(summa_color // self.step) * self.step
 
 img = Image.open("img2.jpg")
-giv_image = np.array(img)
-height = len(giv_image)
-width = len(giv_image[1])
-i = 0
-while i < height:
-    j = 0
-    while j < width:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                r = giv_image[n][n1][0]
-                g = giv_image[n][n1][1]
-                b = giv_image[n][n1][2]
-                color = (int(r) + int(g) + int(b)) / 3
-                s += color
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                giv_image[n][n1][0] = int(s // 50) * 50
-                giv_image[n][n1][1] = int(s // 50) * 50
-                giv_image[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(giv_image)
-res.save('res.jpg')
+image = np.array(img)
+
+results = CreateImage(image, 10, 5).create_image()
+results.save('res.jpg')
