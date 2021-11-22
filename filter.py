1b1c919728f70e02de44a64513167ebc6dee0cc0
AshNRush <72685173+AshNRush@users.noreply.github.com>
2021-11-22 11:41:01 +0600

    Шаг 1
    1. Исправлено переполнение с помощью перехода на int
    2. Введена новая переменная в цикле, переименованы некоторые другие
    3. Фикс расчёта компоненты серого
    4. Фикс расчёта на границах

--- filter.py
+++ filter.py
@@ -2,26 +2,25 @@ from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
 arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+h, w = len(arr), len(arr[1])
 i = 0
-while i < a - 11:
+while i < h:
     j = 0
-    while j < a1 - 11:
+    while j < w:
         s = 0
         for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
+            for m in range(j, j + 10):
+                r = arr[n][m][0]
+                g = arr[n][m][1]
+                b = arr[n][m][2]
+                M = int(r) + int(g) + int(b)
                 s += M
         s = int(s // 100)
         for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
+            for m in range(j, j + 10):
+                arr[n][m][0] = int(s // 50) * 50 / 3
+                arr[n][m][1] = int(s // 50) * 50 / 3
+                arr[n][m][2] = int(s // 50) * 50 / 3
         j = j + 10
     i = i + 10
 res = Image.fromarray(arr)
