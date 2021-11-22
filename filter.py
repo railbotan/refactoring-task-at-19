68649c4056c49251d75ce26f06374c74d7d682db
Dina Popova <91373573+dinosavr17@users.noreply.github.com>
2021-11-22 20:35:00 +0500

    Update filter.py

--- filter.py
+++ filter.py
@@ -2,22 +2,22 @@ from PIL import Image
 import numpy as np
 
 
-class Filter:
+class GrayscaleFilter:
     def __init__(self, p_size, g_step):
         self.pixel_size = p_size
-        self.greyscale = g_step
+        self.grayscale = g_step
         self.img_arr = None
 
-    def average_brightness(self, start_x, start_y):
+    def get_average_brightness(self, start_x, start_y):
         average = np.sum((self.img_arr[start_y: start_y + self.pixel_size, start_x: start_x + self.pixel_size])) / 3
         return int(average // (self.pixel_size * self.pixel_size))
 
-    def gradation_setter(self, start_x, start_y):
+    def set_gradation(self, start_x, start_y):
         average = self.get_average_brightness(start_x, start_y)
         gradation = int(average // self.grayscale) * self.grayscale
         self.img_arr[start_y: start_y + self.pixel_size, start_x: start_x + self.pixel_size] = gradation
 
-    def img_converter(self, input_file_name, output_file_name):
+    def img_convert(self, input_file_name, output_file_name):
         self.img_arr = np.array(Image.open(input_file_name))
         height = len(self.img_arr)
         width = len(self.img_arr[1])
@@ -31,7 +31,7 @@ class Filter:
 input_file_name = input("Введите имя исходного изображения: ")
 output_file_name = input("Введите имя нового изображения: ")
 pixel_size = int(input("Введите размер пикселей: "))
-greyscale_step = int(input("Введите шаг градации серого: "))
+grayscale_step = int(input("Введите шаг градации серого: "))
 
-greyscale_filter = Filter(pixel_size, grayscale_step)
-greyscale_filter.img_convert(input_file_name, output_file_name)
+grayscale_filter = GrayscaleFilter(pixel_size, grayscale_step)
+grayscale_filter.img_convert(input_file_name, output_file_name)
